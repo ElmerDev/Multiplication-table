a9f7c86861eab6e75d5b50bfc2558e060ed51f62
Elmer Raimundo dos Santos <eelmer226@gmail.com>
2020-05-18 17:00:18 -0300

    Arquivos

---
+++ javascript/script.js
@@ -0,0 +1,27 @@
+function calculaTabuada(){
+
+    let tabuada = document.querySelector("#tabuada tbody")
+    let valorA = parseInt(document.querySelector("#valorA").value)
+    tabuada.innerHTML = ''
+    for (let valorB = 0; valorB <= 10; valorB++) {
+
+        let resultado = valorA * valorB
+
+        let template = `
+                <td>${valorA}</td>
+                <td>x</td>
+                <td>${valorB}</td>
+                <td>=</td>
+                <td>${resultado}</td>
+            `
+
+        let tr = document.createElement('tr')
+        tr.innerHTML = template
+        tabuada.append(tr)
+
+    }
+}
+
+calculaTabuada()
+
+document.querySelector("#valorA").addEventListener('change', calculaTabuada)
